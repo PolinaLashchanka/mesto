b00a648d7e87d8698f7a1ac7bb4e44445307dd72
Polina Lashchanka <lisaya666@gmail.com>
2023-02-07 14:14:36 +0100

    added cards with JS

--- scripts/index.js
+++ scripts/index.js
@@ -37,6 +37,56 @@ formElement.addEventListener('submit', handleFormSubmit);
 
 
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+
+  const photoGrid = document.querySelector('.photo-grid');
+  const template = document.querySelector('#template-card');
+
+
+
+  const createCard = (cardName, cardImage) => {
+    const card = template.content.querySelector('.photo-grid__card').cloneNode(true);
+    card.querySelector('.photo-grid__text').textContent = cardName;
+    card.querySelector('.photo-grid__image').src = cardImage;
+    return card;
+  };
+
+  const renderCard = (cardName, cardImage) => {
+    photoGrid.append(createCard(cardName, cardImage));
+  }
+
+  initialCards.forEach((item) => {
+    renderCard(item.name, item.link);
+  });
+
+
+
 
 
 
