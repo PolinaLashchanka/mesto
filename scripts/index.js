f0e31dcbc766e41428d786273a6c13bbf658a971
Polina Lashchanka <lisaya666@gmail.com>
2023-02-08 14:40:29 +0100

    changed location of cards massive

--- scripts/index.js
+++ scripts/index.js
@@ -89,34 +89,6 @@ popupAddCloseButtonElement.addEventListener('click', () => removeVisibility(popu
     photoGrid.prepend(createCard(cardName, cardImage));
   }
 
-  const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
-
-
   initialCards.forEach((item) => {
     renderCard(item.name, item.link);
   });
